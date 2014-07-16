b0062f9cb0aefc49a29a6592656d0c4d3d2b7f4f
EC2 Default User <hadesbox@gmail.com>
2014-07-16 15:05:57 +0000

    fix for POSTs in http

--- mongo-http.js
+++ mongo-http.js
@@ -7,7 +7,7 @@ var url = require('url');
 http.createServer(function (req, res) {
 
 	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
+	query = url_parts.query;
 
 	console.log(url_parts);
 	//console.log(req);
@@ -17,75 +17,91 @@ http.createServer(function (req, res) {
 		res.end("no favicon");
 	}
 	else if(query["proxy"]){
-		//var url_parts = url.parse(query["proxy"], true);
-		var url_parts = url.parse(url_parts.search.substring(7), true);
-		//var url_parts = url_parts.search.substring(7);//.parse(query["proxy"], true);
-		console.log("hola", url_parts);
-		var query = url_parts.query;		
-		if(url_parts.protocol == "http:"){
-			//console.log("is http: call", url_parts.protocol, url_parts.pathname);
-			console.log("parts", url_parts);
-			//console.log("query", query);
-			//console.log("port will be",(url_parts.port != null ? url_parts.port : 80));
-			
-			var options = {
-			  host: url_parts.hostname, 
-			  port: (url_parts.port != null ? url_parts.port : 80),
-			  path: url_parts.path,
-			  method: req.method
-			};
-			proxyBody = "";
-			http.request(options, function(resProxy) {
-			  //console.log('STATUS: ' + resProxy.statusCode);
-			  //console.log('HEADERS: ' + JSON.stringify(resProxy.headers));
-			  resProxy.setEncoding('utf8');
-			  resProxy.on('data', function (chunk) {
-			    proxyBody+=chunk;
-			  });
-			  resProxy.on('end', function (chunk) {
-			    if(undefined == resProxy.headers['access-control-allow-origin']) resProxy.headers['access-control-allow-origin'] = '*';
-			    if(undefined == resProxy.headers['access-control-allow-methods']) resProxy.headers['access-control-allow-methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
-			    if(undefined == resProxy.headers['access-control-allow-headers']) resProxy.headers['access-control-allow-headers'] = 'Content-Type, Authorization, Content-Length, X-Requested-With';
-	                    res.writeHead(resProxy.statusCode, resProxy.headers);
-			    res.end(proxyBody);
-			  });
-			}).end();
-		}
-		else if(url_parts.protocol == "https:"){
-			var options = {
-			  host: url_parts.hostname, 
-			  port: (url_parts.port != null ? url_parts.port : 80),
-			  path: url_parts.pathname,
-			  method: req.method,
-			  rejectUnauthorized: false,
-		      requestCert: true,
-		      agent: false
-			}			
-			proxyBody = "";
-			https.request(options, function(resProxy) {
-			  //console.log('STATUS: ' + resProxy.statusCode);
-			  //console.log('HEADERS: ' + JSON.stringify(resProxy.headers));
-			  resProxy.setEncoding('utf8');
-			  resProxy.on('data', function (chunk) {
-			    proxyBody+=chunk;
-			  });
-			  resProxy.on('end', function (chunk) {
-			    console.log('terminado');
-			    resProxy.headers['Access-Control-Allow-Origin'] = '*';
-			    resProxy.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
-			    resProxy.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, Content-Length, X-Requested-With';
-			    //console.log(resProxy.headers);
-				res.writeHead(resProxy.statusCode, resProxy.headers);
-			    res.end(proxyBody);
-			  });
-			}).end();
+		var bodyRequest = "";
+		req.on("data", function(chunkRequest){
+			//console.log("event data", chunkRequest);
+			bodyRequest+=chunkRequest;
+		});
+		url_parts = url.parse(url_parts.search.substring(7), true);
+		req.on("end", function(){
+			//console.log("event end", url_parts, "fin");
+			//var url_parts = url.parse(query["proxy"], true);
+			//var url_parts = url_parts.search.substring(7);//.parse(query["proxy"], true);
+			//console.log("url_parts", url_parts);
+			var query = url_parts.query;		
+			if(url_parts.protocol == "http:"){
+				//console.log("is http: call", url_parts.protocol, url_parts.pathname);
+				//console.log("query", query);
+				//console.log("port will be",(url_parts.port != null ? url_parts.port : 80));
+				//console.log("headers are", req.headers);
+				var options = {
+				  host: url_parts.hostname, 
+				  port: (url_parts.port != null ? url_parts.port : 80),
+				  path: url_parts.path,
+				  method: req.method 
+				};
+				if(req.method=="POST"){
+					options.headers = { "content-type": "application/json; charset=utf-8", "content-length": bodyRequest.length}; 
+					//console.log("options for request are", options);
+				}
+				proxyBody = "";
+				reqhttp = http.request(options, function(resProxy) {
+				  //console.log('STATUS: ' + resProxy.statusCode);
+				  //console.log('HEADERS: ' + JSON.stringify(resProxy.headers));
+				  resProxy.setEncoding('utf8');
+				  resProxy.on('data', function (chunk) {
+				    proxyBody+=chunk;
+				  });
+				  resProxy.on('end', function () {
+			            //console.log("end");
+				    if(undefined == resProxy.headers['access-control-allow-origin']) resProxy.headers['access-control-allow-origin'] = '*';
+				    if(undefined == resProxy.headers['access-control-allow-methods']) resProxy.headers['access-control-allow-methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
+				    if(undefined == resProxy.headers['access-control-allow-headers']) resProxy.headers['access-control-allow-headers'] = 'Content-Type, Authorization, Content-Length, X-Requested-With';
+				    res.writeHead(resProxy.statusCode, resProxy.headers);
+				    res.end(proxyBody);
+				  });
+				});
+				if(req.method=="POST"){
+					//console.log("body to post is", bodyRequest);
+					reqhttp.write(bodyRequest)
+			 	}	
+				reqhttp.end();
+			}
+			else if(url_parts.protocol == "https:"){
+				var options = {
+				  host: url_parts.hostname, 
+				  port: (url_parts.port != null ? url_parts.port : 80),
+				  path: url_parts.pathname,
+				  method: req.method,
+				  rejectUnauthorized: false,
+			      requestCert: true,
+			      agent: false
+				}			
+				proxyBody = "";
+				https.request(options, function(resProxy) {
+				  //console.log('STATUS: ' + resProxy.statusCode);
+				  //console.log('HEADERS: ' + JSON.stringify(resProxy.headers));
+				  resProxy.setEncoding('utf8');
+				  resProxy.on('data', function (chunk) {
+				    proxyBody+=chunk;
+				  });
+				  resProxy.on('end', function (chunk) {
+				    console.log('terminado');
+				    resProxy.headers['Access-Control-Allow-Origin'] = '*';
+				    resProxy.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
+				    resProxy.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, Content-Length, X-Requested-With';
+				    //console.log(resProxy.headers);
+					res.writeHead(resProxy.statusCode, resProxy.headers);
+				    res.end(proxyBody);
+				  });
+				}).end();
 
-		}
-		else {
-			console.log("protocol Error");
-			res.end("Error");
-		}
-		//retailerpromotions-dev.elasticbeanstalk.com/groups
+			}
+			else {
+				console.log("protocol Error");
+				res.end("Error");
+			}
+		});
 	}
 	else{
 		console.log("looking for file", req.url);
